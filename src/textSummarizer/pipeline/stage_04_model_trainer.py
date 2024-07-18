7e0d52c62cb2e10b72ff77ce122a4ca1737c7138
mayank <mayank26202@gmail.com>
2024-07-18 15:12:52 +0530

    data ingestion notebook added

---
+++ src/textSummarizer/pipeline/stage_04_model_trainer.py
@@ -0,0 +1,27 @@
+from textSummarizer.config.configuration import ConfigurationManager
+from textSummarizer.components.model_trainer import ModelTrainer
+from textSummarizer.logging import logger
+from transformers import TrainingArguments, Trainer, AutoModelForSequenceClassification, AutoTokenizer
+from datasets import load_dataset
+
+
+class ModelTrainerTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer_config = ModelTrainer(config=model_trainer_config)
+        model_trainer_config.train()
+
+
+        trainer_args = TrainingArguments(
+            output_dir=self.config.root_dir, num_train_epochs=1, warmup_steps=500,
+            per_device_train_batch_size=1, per_device_eval_batch_size=1,
+            weight_decay=0.01, logging_steps=10,
+            evaluation_strategy='steps', eval_steps=500, save_steps=1e6,
+            gradient_accumulation_steps=16,
+            dataloader_num_workers=1,  # Set the number of workers to enable multiprocessing
+            dataloader_prefetch_factor=1  # Set the prefetch_factor to a specific value, e.g., 1
+        )
